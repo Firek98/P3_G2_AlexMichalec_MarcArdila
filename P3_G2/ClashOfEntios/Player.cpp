52207668d6a9402c8cd0aea1acbbc6b18c0c9d22
Alex Michalec <alexmc11@hotmail.es>
2017-05-25 11:26:08 +0200

    clase player WIP

--- P3_G2/ClashOfEntios/Player.cpp
+++ P3_G2/ClashOfEntios/Player.cpp
@@ -1,12 +1,67 @@
 #include "Player.h"
+#include <conio.h>
+#include <stdlib.h>
+#include "Input.inl.hh"
+#include "Map.h"
 
 
+char Pj = '@';
 
-Player::Player()
+
+Player::Player(Map m) : map(m)
 {
 }
 
-
 Player::~Player()
 {
+}
+
+void Player::pos()
+{
+
+}
+
+
+void Player::movement(enti::InputKey tecla) //WIP
+{
+	switch (tecla)
+	{
+	case enti::InputKey::NONE:
+		break;
+	case enti::InputKey::W:
+		if (Coords.x > 0) {
+
+			Coords.x--;
+			map.Modify(Coords.x, Coords.y, '@');
+			map.Modify(Coords.x + 1, Coords.y, '.');
+		}
+		break;
+	case enti::InputKey::A:
+		if (Coords.y > 0) {
+	
+			Coords.y--;
+			map.Modify(Coords.x, Coords.y, '@');
+			map.Modify(Coords.x, Coords.y + 1, '.');
+		}
+		break;
+	case enti::InputKey::S:
+		if (Coords.x < (map.rows() - 1)) {
+
+			Coords.x++;
+			map.Modify(Coords.x, Coords.y, '@');
+			map.Modify(Coords.x - 1, Coords.y, '.');
+		}
+		break;
+	case enti::InputKey::D:
+		if (Coords.y < (map.columns() - 1)) {
+
+			Coords.y++;
+			map.Modify(Coords.x, Coords.y, '@');
+			map.Modify(Coords.x, Coords.y - 1, '.');
+		}
+		break;
+
+	default:
+		break;
+	}
 }
--- P3_G2/ClashOfEntios/Player.h
+++ P3_G2/ClashOfEntios/Player.h
@@ -1,8 +1,17 @@
 #pragma once
+#include <conio.h>
+#include "Input.inl.hh"
 class Player
 {
+
 public:
-	Player();
+	Player(Map m);
 	~Player();
+	void pos();
+	void movement(enti::InputKey tecla);
+	Map &map;
+	coord Coords;
+
+};
 };
 
