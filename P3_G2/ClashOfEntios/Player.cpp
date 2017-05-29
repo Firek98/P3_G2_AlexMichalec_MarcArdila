bfc5b2e31dc5c1ee3867f4975614e30197d05e4f
marcardilamillan <marcardilamillan@G4-78>
2017-05-29 13:14:29 +0200

    en processing

--- P3_G2/ClashOfEntios/Player.cpp
+++ P3_G2/ClashOfEntios/Player.cpp
@@ -55,7 +55,7 @@ void Player::movement(enti::InputKey tecla) //WIP
 		}
 		break;
 	case enti::InputKey::A:
-		if (Coords.y > 0 && map.md2(Coords.x - 1, Coords.y) == '.' && map.md2(Coords.x - 1, Coords.y) == ':') {
+		if (Coords.y > 0 && (map.md2(Coords.x - 1, Coords.y) == '.' || map.md2(Coords.x - 1, Coords.y) == ':')) {
 	
 			Coords.y--;
 			map.Modify(Coords.x, Coords.y, value);
@@ -71,11 +71,18 @@ void Player::movement(enti::InputKey tecla) //WIP
 		}
 		break;
 	case enti::InputKey::S:
-		if (Coords.x < (map.rows() - 1) && map.md2(Coords.x, Coords.y + 1) != 'X' && map.md2(Coords.x, Coords.y + 1) != 'O') {
+		if (Coords.x < (map.rows() - 1) && (map.md2(Coords.x, Coords.y + 1) == '.' || map.md2(Coords.x, Coords.y + 1) == ':')) {
 
 			Coords.x++;
 			map.Modify(Coords.x, Coords.y, value);
-			map.Modify(Coords.x - 1, Coords.y, '.');
+			if (map.md2(Coords.x - 1, Coords.y) == '.')
+			{
+				map.Modify(Coords.x - 1, Coords.y, '.');
+			}
+			else if (map.md2(Coords.x - 1, Coords.y) == ':') 
+			{
+				map.Modify(Coords.x - 1, Coords.y, ':');
+			}
 		}
 		break;
 	case enti::InputKey::D:
